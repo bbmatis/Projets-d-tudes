e7465f574644d4da4a9286f32c8369e6f3d32047
bmatis <matis.brun@etu.univ-lyon1.fr>
2022-04-27 15:19:46 +0200

    first upload

---
+++ client.js
@@ -0,0 +1,504 @@
+/* ******************************************************************
+ * Constantes de configuration
+ * ****************************************************************** */
+const apiKey = "46db2fd5-17fe-45e9-bf74-db2b8a37a726";
+const serverUrl = "https://lifap5.univ-lyon1.fr";
+
+/* ******************************************************************
+ * Gestion de la boîte de dialogue (a.k.a. modal) d'affichage de
+ * l'utilisateur.
+ * ****************************************************************** */
+
+/**
+ * Fait une requête GET authentifiée sur /whoami
+ * @returns une promesse du login utilisateur ou du message d'erreur
+ */
+function fetchWhoami() {
+    return fetch(serverUrl + "/whoami", { headers: { "Api-Key": document.getElementById("PasswordAPI").value } })
+        .then((response) => {
+            if (response.status === 401) {
+                return response.json().then((json) => {
+                    console.log(json);
+                    return { err: json.message };
+                });
+            } else {
+                return response.json();
+            }
+        })
+        .catch((erreur) => ({ err: erreur }));
+}
+
+/**
+ * Fait une requête sur le serveur et insère le login dans la modale d'affichage
+ * de l'utilisateur puis déclenche l'affichage de cette modale.
+ *
+ * @param {Etat} etatCourant l'état courant
+ * @returns Une promesse de mise à jour
+ */
+function lanceWhoamiEtInsereLogin(etatCourant) {
+    return fetchWhoami().then((data) => {
+        majEtatEtPage(etatCourant, {
+            login: data.user, // qui vaut undefined en cas d'erreur
+            errLogin: data.err, // qui vaut undefined si tout va bien
+            loginModal: false, // on affiche la modale
+        });
+        console.log("User :",data.user)
+    });
+}
+
+
+/**
+ * Génère le code HTML du corps de la modale de login. On renvoie en plus un
+ * objet callbacks vide pour faire comme les autres fonctions de génération,
+ * mais ce n'est pas obligatoire ici.
+ * @param {Etat} etatCourant
+ * @returns un objet contenant le code HTML dans le champ html et un objet vide
+ * dans le champ callbacks
+ */
+function genereModaleLoginBody(etatCourant) {
+    const text =
+        etatCourant.errLogin !== undefined ?
+        etatCourant.errLogin :
+        etatCourant.login;
+    return {
+        html: `
+  <section class="modal-card-body">
+    <div class="field>
+      <label class="label"> <b>Clé d'API</b> <label>
+      <input id="PasswordAPI" type="password" class="input" value>
+    </div>
+  </section>
+  `,
+        callbacks: {},
+    };
+}
+
+/**
+ * Génère le code HTML du titre de la modale de login et les callbacks associés.
+ *
+ * @param {Etat} etatCourant
+ * @returns un objet contenant le code HTML dans le champ html et la description
+ * des callbacks à enregistrer dans le champ callbacks
+ */
+function genereModaleLoginHeader(etatCourant) {
+    return {
+        html: `
+<header class="modal-card-head  is-back">
+  <p class="modal-card-title">Utilisateur</p>
+  <button
+    id="btn-close-login-modal1"
+    class="delete"
+    aria-label="close"
+    >
+  </button>
+</header>`,
+callbacks: {
+            "btn-close-login-modal1": {
+                onclick: () => majEtatEtPage(etatCourant, { loginModal: false }),
+            },
+        },
+    };
+}
+
+/**
+ * Génère le code HTML du base de page de la modale de login et les callbacks associés.
+ *
+ * @param {Etat} etatCourant
+ * @returns un objet contenant le code HTML dans le champ html et la description
+ * des callbacks à enregistrer dans le champ callbacks
+ */
+function genereModaleLoginFooter(etatCourant) {
+    return {
+        html: `
+  <footer class="modal-card-foot" style="justify-content: flex-end">
+    <button id="btn-fermer" class="is-danger button">Fermer</button>
+    <button id="btn-valider" class="is-success button">Valider</button>
+  </footer>
+  `,
+        callbacks: {
+            "btn-fermer": {
+                onclick: () => majEtatEtPage(etatCourant, { loginModal: false }),
+            },
+            "btn-valider": {
+              onclick: () => lanceWhoamiEtInsereLogin(etatCourant),
+            },
+        },
+    };
+}
+
+/**
+ * Génère le code HTML de la modale de login et les callbacks associés.
+ *
+ * @param {Etat} etatCourant
+ * @returns un objet contenant le code HTML dans le champ html et la description
+ * des callbacks à enregistrer dans le champ callbacks
+ */
+function genereModaleLogin(etatCourant) {
+    const header = genereModaleLoginHeader(etatCourant);
+    const footer = genereModaleLoginFooter(etatCourant);
+    const body = genereModaleLoginBody(etatCourant);
+    const activeClass = etatCourant.loginModal ? "is-active" : "is-inactive";
+    return {
+        html: `
+      <div id="mdl-login" class="modal ${activeClass}">
+        <div class="modal-background"></div>
+        <div class="modal-card">
+          ${header.html}
+          ${body.html}
+          ${footer.html}
+        </div>
+      </div>`,
+        callbacks: {...header.callbacks, ...footer.callbacks, ...body.callbacks },
+    };
+}
+
+/* ************************************************************************
+ * Gestion de barre de navigation contenant en particulier les bouton Pokedex,
+ * Combat et Connexion.
+ * ****************************************************************** */
+
+/**
+ * Déclenche la mise à jour de la page en changeant l'état courant pour que la
+ * modale de login soit affichée
+ * @param {Etat} etatCourant
+ */
+function afficheModaleConnexion(etatCourant) {
+    return majEtatEtPage(etatCourant,{loginModal: true});
+}
+
+/**
+ * Génère le code HTML et les callbacks pour la partie droite de la barre de
+ * navigation qui contient le bouton de login.
+ * @param {Etat} etatCourant
+ * @returns un objet contenant le code HTML dans le champ html et la description
+ * des callbacks à enregistrer dans le champ callbacks
+ */
+function genereBoutonConnexion(etatCourant) {
+  if(etatCourant.login === undefined) {
+    const html = `
+  <div class="navbar-end">
+    <div class="navbar-item">
+      <div class="buttons">
+        <a id="btn-connect" class="button is-light"> Connexion </a>
+      </div>
+    </div>
+  </div>`;
+    return {
+        html: html,
+        callbacks: {
+            "btn-connect": {
+                onclick: () => afficheModaleConnexion(etatCourant),
+            },
+        },
+    };
+  }
+}
+
+function genereBoutonDeconnexion(etatCourant) {
+  if(!etatCourant.login === undefined) {
+    const htmlDeco = `
+  <div class="navbar-end">
+    <div class="navbar-item">
+      <div class="buttons">
+        <a id="btn-disconnect" class="is-danger button"> Déconnexion </a>
+      </div>
+    </div>
+  </div>`;
+    return {
+        html: htmlDeco,
+        callbacks: {
+            "btn-disconnect": {
+                onclick: () => majEtatEtPage(etatCourant,{login: undefined}),
+            },
+        },
+    };
+  }
+}
+
+/**
+ * Génère le code HTML de la barre de navigation et les callbacks associés.
+ * @param {Etat} etatCourant
+ * @returns un objet contenant le code HTML dans le champ html et la description
+ * des callbacks à enregistrer dans le champ callbacks
+ */
+function genereBarreNavigation(etatCourant) {
+    const deconnexion = genereBoutonDeconnexion(etatCourant);
+    const connexion = genereBoutonConnexion(etatCourant);
+    return {
+        html: `
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+          <div class="navbar">
+            <div class="navbar-item"><div class="buttons">
+                <a id="btn-pokedex" class="button is-light"> Pokedex </a>
+                <a id="btn-combat" class="button is-light"> Combat </a>
+            </div></div>
+            ${connexion.html}
+          </div>
+        </nav>`,
+              callbacks: {
+                  ...connexion.callbacks,
+                  "btn-pokedex": { onclick: () => console.log("click bouton pokedex") },
+              },
+      };
+
+}
+
+function genereListePokemon(etatCourant) {
+    //on crée un tableau avec html pour chaque ligne du tableau
+    const ligneTableau = etatCourant.pokemons.map((pokemon) => `<tr id="pokemon-${pokemon.PokedexNumber}" class=" ${etatCourant.pokemon && etatCourant.pokemon.PokedexNumber == pokemon.PokedexNumber ? "is-selected" : "" }">
+  <td><img src="${pokemon.Images.Detail}" alt="${pokemon.Name}"/></td>
+  <td>${pokemon.PokedexNumber}</td>
+  <td>${pokemon.Name}</td>
+  <td>${pokemon.Abilities.join("</br>")}</td>
+  <td>${pokemon.Types.join("</br>")}</td>
+  </tr>`).join("")
+        //on crée un tableau avec les callbacks pour chaque pokemon du tableau
+    const callbacks = etatCourant.pokemons.map((pokemon) => ({
+        [`pokemon-${pokemon.PokedexNumber}`]: {
+            onclick: () => {
+                console.log("click sur un pokemon", pokemon.PokedexNumber);
+                majEtatEtPage(etatCourant, { pokemon: pokemon });
+            },
+        },
+    }))
+    const html = `<table class="table is-fullwidth">
+                  <thead>
+                      <tr>
+                          <th>Image</th>
+                          <th>#<i class="fas fa-angle-up"></i></th>
+                          <th>Name</th>
+                          <th>Abilities</th>
+                          <th>Types</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                      ${ligneTableau}
+                  </tbody>
+                </table>`
+    return {
+        html: html,
+        //On fusionne les callbacks grâce au reduce  
+        callbacks: callbacks.reduce((acc, cur) => ({...acc, ...cur }), {})
+    }
+}
+
+function genereInfoPokemon(etatCourant) {
+    if (!etatCourant.pokemon) return { html: "pokemonInfo", callbacks: {} };
+    const pokemon = etatCourant.pokemon;
+    const html = `<div class="card">
+                  <div class="card-header">
+                    <div class="card-header-title">${pokemon.JapaneseName} (#${pokemon.PokedexNumber})</div>
+                  </div>
+                  <div class="card-content">
+                    <article class="media">
+                      <div class="media-content">
+                        <h1 class="title">${pokemon.name}</h1>
+                      </div>
+                    </article>
+                  </div>
+                  <div class="card-content">
+                    <article class="media">
+                      <div class="media-content">
+                        <div class="content has-text-left">
+                          <p>Hit points: ${pokemon.Attack} </p>
+                          <h3>Abilities</h3>
+                          <ul>
+                            <li> ${pokemon.Abilities.join("<li></li>")} </li>
+                          </ul>
+                          <h3>Resistant against</h3>
+                          <ul>
+                            <li> ${Object.keys(pokemon.Against).filter(x => pokemon.Against[x] < 1).join("<li></li>")} </li>
+                          </ul>
+                          <h3>Weak against</h3>
+                          <ul>
+                            <li> ${Object.keys(pokemon.Against).filter(x => pokemon.Against[x] > 1).join("<li></li>")} </li>
+                          </ul>
+                        </div>
+                      </div>
+                      <figure class="media-right">
+                        <figure class="image is-475x475">
+                          <img
+                            class=""
+                            src="${pokemon.Images.Full}"
+                            alt="${pokemon.name}"
+                          />
+                        </figure>
+                      </figure>
+                    </article>
+                  </div>
+                  <div class="card-footer">
+                    <article class="media">
+                      <div class="media-content">
+                        <button class="is-success button" tabindex="0">
+                          Ajouter à mon deck
+                        </button>
+                      </div>
+                    </article>
+                  </div>
+                </div>`
+
+    return {
+        html: html,
+        callbacks: {}
+    }
+}
+
+function generePagePokedex(etatCourant) {
+    const tabPokemon = genereListePokemon(etatCourant)
+    const pokemonInfo = genereInfoPokemon(etatCourant)
+    const html = `<div class="columns">
+                  <div class="column">
+                    <div class="tabs is-centered">
+                      <ul>
+                        <li class="is-active" id="tab-all-pokemons">
+                          <a>Tous les pokemons</a>
+                        </li>
+                        <li id="tab-tout"><a>Mes pokemons</a></li>
+                      </ul>
+                    </div>
+                    <div id="tbl-pokemons">
+                      ${tabPokemon.html}
+                    </div>
+                  </div>
+                  <div class="column">
+                    ${pokemonInfo.html}
+                  </div>
+                </div>`
+
+    return {
+        html: html,
+        callbacks: tabPokemon.callbacks
+    }
+}
+
+function genereCorpsPage(etatCourant) {
+    return generePagePokedex(etatCourant);
+}
+
+//fonction recupere liste poké sur serveur avec requete fetch sur API
+async function getPokemonList() {
+    return fetch(serverUrl + "/pokemon")
+        .then(async(response) => { // async pour pouvoir utiliser await
+            if (response.status == 200) {
+                const data = await response.json() // await attend la reponse et data contient la liste des pokemons
+                return data;
+            } else {
+                return []; //sinon on retourne un tab vide
+            }
+        })
+        .catch([]);
+}
+
+/**
+ * Génére le code HTML de la page ainsi que l'ensemble des callbacks à
+ * enregistrer sur les éléments de cette page.
+ *
+ * @param {Etat} etatCourant
+ * @returns un objet contenant le code HTML dans le champ html et la description
+ * des callbacks à enregistrer dans le champ callbacks
+ */
+function generePage(etatCourant) {
+    const barredeNavigation = genereBarreNavigation(etatCourant);
+    const modaleLogin = genereModaleLogin(etatCourant);
+    const corpsPage = genereCorpsPage(etatCourant);
+    // remarquer l'usage de la notation ... ci-dessous qui permet de "fusionner"
+    // les dictionnaires de callbacks qui viennent de la barre et de la modale.
+    // Attention, les callbacks définis dans modaleLogin.callbacks vont écraser
+    // ceux définis sur les mêmes éléments dans barredeNavigation.callbacks. En
+    // pratique ce cas ne doit pas se produire car barreDeNavigation et
+    // modaleLogin portent sur des zone différentes de la page et n'ont pas
+    // d'éléments en commun.
+    return {
+        html: barredeNavigation.html + modaleLogin.html + corpsPage.html,
+        callbacks: {...barredeNavigation.callbacks, ...modaleLogin.callbacks, ...corpsPage.callbacks },
+    };
+}
+
+/* ******************************************************************
+ * Initialisation de la page et fonction de mise à jour
+ * globale de la page.
+ * ****************************************************************** */
+
+/**
+ * Créée un nouvel état basé sur les champs de l'ancien état, mais en prenant en
+ * compte les nouvelles valeurs indiquées dans champsMisAJour, puis déclenche la
+ * mise à jour de la page et des événements avec le nouvel état.
+ *
+ * @param {Etat} etatCourant etat avant la mise à jour
+ * @param {*} champsMisAJour objet contenant les champs à mettre à jour, ainsi
+ * que leur (nouvelle) valeur.
+ */
+function majEtatEtPage(etatCourant, champsMisAJour) {
+    const nouvelEtat = {...etatCourant, ...champsMisAJour };
+    majPage(nouvelEtat);
+}
+
+
+/**
+ * Prend une structure décrivant les callbacks à enregistrer et effectue les
+ * affectation sur les bon champs "on...". Par exemple si callbacks contient la
+ * structure suivante où f1, f2 et f3 sont des callbacks:
+ *
+ * { "btn-pokedex": { "onclick": f1 },
+ *   "input-search": { "onchange": f2,
+ *                     "oninput": f3 }
+ * }
+ *
+ * alors cette fonction rangera f1 dans le champ "onclick" de l'élément dont
+ * l'id est "btn-pokedex", rangera f2 dans le champ "onchange" de l'élément dont
+ * l'id est "input-search" et rangera f3 dans le champ "oninput" de ce même
+ * élément. Cela aura, entre autres, pour effet de délclencher un appel à f1
+ * lorsque l'on cliquera sur le bouton "btn-pokedex".
+ *
+ * @param {Object} callbacks dictionnaire associant les id d'éléments à un
+ * dictionnaire qui associe des champs "on..." aux callbacks désirés.
+ */
+function enregistreCallbacks(callbacks) {
+    Object.keys(callbacks).forEach((id) => {
+        const elt = document.getElementById(id);
+        if (elt === undefined || elt === null) {
+            console.log(
+                `Élément inconnu: ${id}, impossible d'enregistrer de callback sur cet id`
+            );
+        } else {
+            Object.keys(callbacks[id]).forEach((onAction) => {
+                elt[onAction] = callbacks[id][onAction];
+            });
+        }
+    });
+}
+
+/**
+ * Mets à jour la page (contenu et événements) en fonction d'un nouvel état.
+ *
+ * @param {Etat} etatCourant l'état courant
+ */
+function majPage(etatCourant) {
+    console.log("CALL majPage");
+    const page = generePage(etatCourant);
+    document.getElementById("root").innerHTML = page.html;
+    enregistreCallbacks(page.callbacks);
+}
+
+/**
+ * Appelé après le chargement de la page.
+ * Met en place la mécanique de gestion des événements
+ * en lançant la mise à jour de la page à partir d'un état initial.
+ */
+async function initClientPokemons() {
+    console.log("CALL initClientPokemons");
+    const etatInitial = {
+        loginModal: false,
+        login: undefined,
+        errLogin: undefined,
+        pokemons: await getPokemonList(),
+    };
+    console.log("init", etatInitial)
+    majPage(etatInitial);
+}
+
+// Appel de la fonction init_client_duels au après chargement de la page
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Exécution du code après chargement de la page");
+    initClientPokemons();
+});
